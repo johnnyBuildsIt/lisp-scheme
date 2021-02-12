2a437c3258d88801167f0c165bcf83bcd031c6d5
johnnybuildsit <johnnypr@udel.edu>
2021-02-11 22:29:45 -0500

    sqrt in lexical scope

--- sqrt.lisp
+++ sqrt.lisp
@@ -15,4 +15,22 @@
 (define (sqrt-solver x)
 	(sqrt-recursive 1.0 x))
 
-(sqrt-solver 7)
+;;(sqrt-solver 7)
+
+(define (sqrt-block x )
+	
+	(define (good-enough? guess actual)
+		(< (abs (- actual (square guess))) .001))
+	
+	(define (improve-guess guess quotient)
+		(/ (+ guess quotient) 2))
+
+	(define (sqrt-recursive guess input)
+		(if (good-enough? guess input)
+			guess
+			(sqrt-recursive (improve-guess guess (/ input guess)) input)))
+
+	(sqrt-recursive 1.0 x)
+)
+
+(sqrt-block 9)
